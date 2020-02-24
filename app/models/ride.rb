50a340592a8ac3525d6dc139b8f40a3245fae889
Lindsey Wells <github email address>
2020-02-24 11:46:34 +0000

    add missing associations to models

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,4 +1,5 @@
 class Ride < ApplicationRecord
+  has_many :user_rides
   belongs_to :tribe
   has_many :users, through: :user_rides
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :user_rides
+  has_many :user_tribes
   has_many :tribes, through: :user_tribes
   has_many :rides, through: :user_rides
 end
