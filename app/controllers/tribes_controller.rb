3768bc3c02b116885d0a32e5033f2541b7b7e599
Lindsey Wells <github email address>
2020-02-24 20:38:44 +0000

    create tribe create form and method

--- app/controllers/tribes_controller.rb
+++ app/controllers/tribes_controller.rb
@@ -2,4 +2,21 @@ class TribesController < ApplicationController
   def index
     @tribes = Tribe.all
   end
+
+  def new
+    @tribe = Tribe.new
+  end
+
+  def create
+    @tribe=Tribe.new(tribe_params)
+    @tribe.save
+    redirect_to tribe_path(@tribe)
+  end
+
+  private
+
+  def tribe_params
+    params.require(:tribe).permit(:name, :image_url, :url)
+    end
+
 end
