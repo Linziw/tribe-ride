ed4f4a278c2f669af282c107035188bd4fd4991a
Lindsey Wells <github email address>
2020-02-24 22:06:42 +0000

    add tickboxes to join tribes page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,6 @@ class SessionsController < ApplicationController
   def create
   @user= User.find_by_username(params[:username])
     if @user && @user.authenticate(params[:password])
-      "yep thats them alright"
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
