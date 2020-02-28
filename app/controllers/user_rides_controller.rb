b81176c7d521e0897aaf8d35954a4d5c43c22fb6
Lindsey Wells <github email address>
2020-02-28 15:04:44 +0000

    add destroy method to userrides controller

--- app/controllers/user_rides_controller.rb
+++ app/controllers/user_rides_controller.rb
@@ -1,2 +1,15 @@
 class UserRidesController < ApplicationController
+
+  def destroy
+    @user=User.find(session[:user_id])
+    @ride=Ride.find(params[:id])
+  
+    @user.rides.delete(@ride)
+    @ride.users.delete(@user)
+
+    @user.save
+    @ride.save
+    redirect_to user_path(@user)
+  end
+
 end
