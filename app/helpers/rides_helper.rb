eeb5ee647dbb71cb8d8eb7561dfe9591b7e35617
Lindsey Wells <github email address>
2020-03-04 21:29:52 +0000

    add helper method for instructor names

--- app/helpers/rides_helper.rb
+++ app/helpers/rides_helper.rb
@@ -4,6 +4,11 @@ module RidesHelper
     ride.users.count
   end
 
+  def instructors 
+    ["Alex Toussant", "Ally Love", "Ben Alldis", "Christine D'Ercole", "Cody Rigsby", "Denis Morton", "Emma Lovewell", "Erik Jager", "Hannah Marie Corbin", "Hannah Frankson", "Irene Scholz", "Jenn Sherman", "Jess King", "Kendall Toole", "Leanne Hainsby", "Matt Wilpers", 
+  "Olivia Amato", "Robin Arzon", "Sam Yo", "Tunde Oyeneyin"]
+    end
+
   
 
 end
